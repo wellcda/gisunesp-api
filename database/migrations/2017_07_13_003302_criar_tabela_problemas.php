331130e51f52c3e8395592e2f451b23f2747f789
Wellington Aguiar <wellington.aguiar@institutosoma.org.br>
2017-07-12 22:56:45 -0300

    Migration de tabela problemas e tipos_problemas

---
+++ database/migrations/2017_07_13_003302_criar_tabela_problemas.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CriarTabelaProblemas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipos_problemas' , function(Blueprint $table) {
+            $table->increments('id');
+            $table->text('descricao');
+            $table->string('tipo_geom');
+        });
+
+        Schema::create('problemas' , function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('usuario_id');
+            $table->integer('tipo_problema_id');
+            $table->text('descricao');
+            $table->string('geom');
+            $table->boolean('resolvido');
+
+            $table->foreign('tipo_problema_id')->references('id')->on('tipos_problemas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tipos_problemas');
+        Schema::drop('problemas');
+    }
+}
